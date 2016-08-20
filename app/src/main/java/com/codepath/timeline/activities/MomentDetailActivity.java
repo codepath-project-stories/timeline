f979388a7437cf27430d14174d4f3e36d78d9bf8
Ching-yao Liu <leoliu1313@gmail.com>
2016-08-20 12:18:41 -0700

    have a question

--- app/src/main/java/com/codepath/timeline/activities/MomentDetailActivity.java
+++ app/src/main/java/com/codepath/timeline/activities/MomentDetailActivity.java
@@ -1,27 +1,18 @@
 package com.codepath.timeline.activities;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
-import android.support.v4.app.FragmentTransaction;
-import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.widget.FrameLayout;
 
-import com.ToxicBakery.viewpager.transforms.CubeInTransformer;
 import com.ToxicBakery.viewpager.transforms.CubeOutTransformer;
-import com.ToxicBakery.viewpager.transforms.RotateUpTransformer;
 import com.codepath.timeline.R;
 import com.codepath.timeline.fragments.MomentDetailFragment;
 import com.codepath.timeline.models.Moment;
 import com.codepath.timeline.util.AppConstants;
 import com.codepath.timeline.util.MockResponseGenerator;
-import com.codepath.timeline.util.view.DepthPageTransformer;
-import com.codepath.timeline.util.view.ZoomOutPageTransformer;
-
-import org.parceler.Parcels;
 
 import java.util.List;
 
@@ -29,6 +20,13 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class MomentDetailActivity extends AppCompatActivity {
+  // TODO: not sure if the flow is the following
+  // it seems showDetailDialog() generates DetailDialogFragment
+  // DetailDialogFragment creates R.layout.fragment_moment_detail
+  // so... this activity is not used?
+  // but it is still left/right scrollable
+  // looks like it has ScreenSlidePagerAdapter
+
   @BindView(R.id.vpMoment)
   ViewPager vpMoment;
 
