64f18eb2857730c5104686e5910bdeba60590c03
Marusya82 <marina.tanasyuk@gmail.com>
2016-08-21 21:30:25 -0700

    Adding back the commented

--- app/src/main/java/com/codepath/timeline/fragments/UserStoriesFragment.java
+++ app/src/main/java/com/codepath/timeline/fragments/UserStoriesFragment.java
@@ -3,9 +3,14 @@ package com.codepath.timeline.fragments;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.util.Log;
 
 import com.codepath.timeline.models.Story;
 import com.codepath.timeline.network.TimelineClient;
+import com.codepath.timeline.util.ParseApplication;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
 
 import java.util.List;
 
@@ -31,48 +36,46 @@ public class UserStoriesFragment extends BaseStoryModelFragment {
     @Override
     protected void populateList() {
 
-        List<Story> stories = TimelineClient.getInstance().getMockStoryList(getContext());
-        addAll(stories);
+//        List<Story> stories = TimelineClient.getInstance().getMockStoryList(getContext());
+//        addAll(stories);
 
-//        ParseUser currentUser = ParseUser.getCurrentUser();
-//        String demoCreatedString = (String) currentUser.get("demoCreated");
-//        boolean demoCreated = demoCreatedString != null && demoCreatedString.equals("true");
-//        if (!ParseApplication.DEMO_MODE || (ParseApplication.DEMO_MODE && demoCreated)) {
-//            // demo already created
-//            // fetch stories from Parse server
-//            Log.d("populateList", "getStoryList");
-//            TimelineClient.getInstance().getStoryList(
-//                    getActivity(),
-//                    currentUser,
-//                    // set up callback
-//                    new TimelineClient.TimelineClientGetStoryListener() {
-//                        @Override
-//                        public void onGetStoryList(List<Story> itemList) {
-//                            addAll(itemList);
-//                        }
-//                    });
-//        }
-//        else if (ParseApplication.DEMO_MODE && !demoCreated) {
-//            // demo not created yet
-//            // create fake mock stories
-//            Log.d("populateList", "getMockStoryList");
-//            addAll(TimelineClient.getInstance().getMockStoryList(getActivity()));
-//            currentUser.put("demoCreated", "true");
-//            currentUser.saveInBackground(new SaveCallback() {
-//                @Override
-//                public void done(ParseException e) {
-//                    if (e == null) {
-//                        Log.d("demoCreated", "done success");
-//                    } else {
-//                        // Sign up didn't succeed. Look at the ParseException
-//                        // to figure out what went wrong
-//                        Log.d("demoCreated", "done failed");
-//                        Log.d("demoCreated", e.toString());
-//                    }
-//                }
-//            });
-//        }
-
-
+        ParseUser currentUser = ParseUser.getCurrentUser();
+        String demoCreatedString = (String) currentUser.get("demoCreated");
+        boolean demoCreated = demoCreatedString != null && demoCreatedString.equals("true");
+        if (!ParseApplication.DEMO_MODE || (ParseApplication.DEMO_MODE && demoCreated)) {
+            // demo already created
+            // fetch stories from Parse server
+            Log.d("populateList", "getStoryList");
+            TimelineClient.getInstance().getStoryList(
+                    getActivity(),
+                    currentUser,
+                    // set up callback
+                    new TimelineClient.TimelineClientGetStoryListener() {
+                        @Override
+                        public void onGetStoryList(List<Story> itemList) {
+                            addAll(itemList);
+                        }
+                    });
+        }
+        else if (ParseApplication.DEMO_MODE && !demoCreated) {
+            // demo not created yet
+            // create fake mock stories
+            Log.d("populateList", "getMockStoryList");
+            addAll(TimelineClient.getInstance().getMockStoryList(getActivity()));
+            currentUser.put("demoCreated", "true");
+            currentUser.saveInBackground(new SaveCallback() {
+                @Override
+                public void done(ParseException e) {
+                    if (e == null) {
+                        Log.d("demoCreated", "done success");
+                    } else {
+                        // Sign up didn't succeed. Look at the ParseException
+                        // to figure out what went wrong
+                        Log.d("demoCreated", "done failed");
+                        Log.d("demoCreated", e.toString());
+                    }
+                }
+            });
+        }
     }
 }
