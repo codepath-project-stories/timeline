615ed416e380d12911bad464d598600c245b812c
Marusya82 <marina.tanasyuk@gmail.com>
2016-08-24 17:49:54 -0700

    Implemented basic flow for adding a story

---
+++ app/src/main/java/com/codepath/timeline/activities/NewStoryActivity.java
@@ -0,0 +1,293 @@
+package com.codepath.timeline.activities;
+
+import android.Manifest;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.codepath.timeline.R;
+import com.codepath.timeline.models.Story;
+
+import org.parceler.Parcels;
+
+import java.io.File;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import permissions.dispatcher.NeedsPermission;
+import permissions.dispatcher.OnNeverAskAgain;
+import permissions.dispatcher.OnPermissionDenied;
+import permissions.dispatcher.OnShowRationale;
+import permissions.dispatcher.PermissionRequest;
+import permissions.dispatcher.RuntimePermissions;
+
+@RuntimePermissions
+public class NewStoryActivity extends AppCompatActivity {
+
+    @BindView(R.id.ivBackground) ImageView ivBackground;
+    @BindView(R.id.ivCameraIcon) ImageView ivCameraIcon;
+    @BindView(R.id.etAddTitle) EditText etStoryTitle;
+    @BindView(R.id.tvAddLocation) TextView tvAddLocation;
+    @BindView(R.id.tvAddPeople) TextView tvAddPeople;
+    @BindView(R.id.flStoryPhoto) FrameLayout flStoryPhoto;
+
+    public final String APP_TAG = "TimelineApp";
+    public final static int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 1034;
+    public String photoFileName = "photo.jpg";
+    private Uri takenPhotoUri;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_newstory);
+        ButterKnife.bind(this);
+    }
+
+    // Todo: https://developer.android.com/training/camera/photobasics.html
+    // https://developer.android.com/training/camera/cameradirect.html
+    public void addPhoto(View view) {
+        Snackbar.make(findViewById(android.R.id.content), "clicked", Snackbar.LENGTH_SHORT).show();
+        onLaunchCamera(view);
+    }
+
+    public void publish(View view) {
+        Snackbar.make(findViewById(android.R.id.content), "clicked", Snackbar.LENGTH_SHORT).show();
+        if (etStoryTitle.getText().length() != 0 && ivBackground.getDrawable() != null) {
+            // create a story
+            Story story = new Story();
+            story.setBackgroundImageUrl(takenPhotoUri.getPath());
+            story.setTitle(etStoryTitle.getText().toString());
+            Log.d("DEBUG", story.toString());
+
+            // send result back
+            Intent data = new Intent();
+            data.putExtra("story", Parcels.wrap(story));
+            setResult(1, data);
+
+            // closes the activity, pass data to parent
+            finish();
+            // Todo: add collaborators, add location
+        } else {
+            Snackbar.make(findViewById(android.R.id.content), "Fill out required fields", Snackbar.LENGTH_SHORT).show();
+        }
+    }
+
+    // Todo: retrieve current address https://developer.android.com/training/location/display-address.html
+    public void addLocation(View view) {
+        Snackbar.make(findViewById(android.R.id.content), "clicked", Snackbar.LENGTH_SHORT).show();
+    }
+
+    public void addPeople(View view) {
+        Snackbar.make(findViewById(android.R.id.content), "clicked", Snackbar.LENGTH_SHORT).show();
+    }
+
+    public void onLaunchCamera(View view) {
+        // create Intent to take a picture and return control to the calling application
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, getPhotoFileUri(photoFileName)); // set the image file name
+
+        // If you call startActivityForResult() using an intent that no app can handle, your app will crash.
+        // So as long as the result is not null, it's safe to use the intent.
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            // Start the image capture intent to take photo
+            startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                takenPhotoUri = getPhotoFileUri(photoFileName);
+                // by this point we have the camera photo on disk
+                Bitmap takenImage = BitmapFactory.decodeFile(takenPhotoUri.getPath());
+                // RESIZE BITMAP, see section below
+                // Load the taken image into a preview
+                ivBackground.setImageBitmap(takenImage);
+            } else { // Result was a failure
+                Toast.makeText(this, "Picture wasn't taken!", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    // Returns the Uri for a photo stored on disk given the fileName
+    public Uri getPhotoFileUri(String fileName) {
+        // Only continue if the SD Card is mounted
+        if (isExternalStorageAvailable()) {
+            // Get safe storage directory for photos
+            // Use `getExternalFilesDir` on Context to access package-specific directories.
+            // This way, we don't need to request external read/write runtime permissions.
+            File mediaStorageDir = new File(
+                    getExternalFilesDir(Environment.DIRECTORY_PICTURES), APP_TAG);
+
+            // Create the storage directory if it does not exist
+            if (!mediaStorageDir.exists() && !mediaStorageDir.mkdirs()){
+                Log.d(APP_TAG, "failed to create directory");
+            }
+
+            // Return the file target for the photo based on filename
+            return Uri.fromFile(new File(mediaStorageDir.getPath() + File.separator + fileName));
+        }
+        return null;
+    }
+
+    // Returns true if external storage for photos is available
+    private boolean isExternalStorageAvailable() {
+        String state = Environment.getExternalStorageState();
+        return state.equals(Environment.MEDIA_MOUNTED);
+    }
+
+    // Todo: all these permissions are must for API 23, put in a separate class if reused
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        NewStoryActivityPermissionsDispatcher.onRequestPermissionsResult(this, requestCode, grantResults);
+    }
+
+    @NeedsPermission(Manifest.permission.ACCESS_COARSE_LOCATION)
+    void accessLocation() {
+        // Trigger the calling of a number here
+    }
+
+    @NeedsPermission(Manifest.permission.CAMERA)
+    void openCamera() {
+        // Trigger the calling of a number here
+    }
+
+    @NeedsPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
+    void readStorage() {
+        // Trigger the reading storage
+    }
+
+    @NeedsPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+    void writeStorage() {
+        // Trigger the writing to a storage
+    }
+
+    @OnShowRationale(Manifest.permission.READ_EXTERNAL_STORAGE)
+    void showRationaleToAccessStorage(final PermissionRequest request) {
+        new AlertDialog.Builder(this)
+                .setMessage(R.string.permission_access_storage_rationale)
+                .setPositiveButton(R.string.button_allow, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog,int id) {
+                        request.proceed();
+                    }
+                })
+                .setNegativeButton(R.string.button_deny, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog,int id) {
+                        request.cancel();
+                    }
+                })
+                .show();
+    }
+
+    @OnShowRationale(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+    void showRationaleToWriteStorage(final PermissionRequest request) {
+        new AlertDialog.Builder(this)
+                .setMessage(R.string.permission_write_storage_rationale)
+                .setPositiveButton(R.string.button_allow, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog,int id) {
+                        request.proceed();
+                    }
+                })
+                .setNegativeButton(R.string.button_deny, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog,int id) {
+                        request.cancel();
+                    }
+                })
+                .show();
+    }
+
+    @OnShowRationale(Manifest.permission.ACCESS_COARSE_LOCATION)
+    void showRationaleAccessLocation(final PermissionRequest request) {
+        new AlertDialog.Builder(this)
+                .setMessage(R.string.permission_access_location_rationale)
+                .setPositiveButton(R.string.button_allow, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog,int id) {
+                        request.proceed();
+                    }
+                })
+                .setNegativeButton(R.string.button_deny, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog,int id) {
+                        request.cancel();
+                    }
+                })
+                .show();
+    }
+
+    @OnShowRationale(Manifest.permission.CAMERA)
+    void showRationaleForCamera(final PermissionRequest request) {
+        new AlertDialog.Builder(this)
+                .setMessage(R.string.permission_camera_rationale)
+                .setPositiveButton(R.string.button_allow, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog,int id) {
+                        request.proceed();
+                    }
+                })
+                .setNegativeButton(R.string.button_deny, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog,int id) {
+                        request.cancel();
+                    }
+                })
+                .show();
+    }
+
+    // Annotate a method which is invoked if the user doesn't grant the permissions
+    @OnPermissionDenied(Manifest.permission.ACCESS_COARSE_LOCATION)
+    void showDeniedForLocation() {
+        Snackbar.make(findViewById(android.R.id.content), R.string.permission_denied, Snackbar.LENGTH_SHORT).show();
+    }
+
+    // Annotates a method which is invoked if the user
+    // chose to have the device "never ask again" about a permission
+    @OnNeverAskAgain(Manifest.permission.ACCESS_COARSE_LOCATION)
+    void showNeverAskForLocation() {
+        Snackbar.make(findViewById(android.R.id.content), R.string.permission_neverask, Snackbar.LENGTH_SHORT).show();
+    }
+
+    @OnPermissionDenied(Manifest.permission.CAMERA)
+    void showDeniedForCamera() {
+        Snackbar.make(findViewById(android.R.id.content), R.string.permission_denied, Snackbar.LENGTH_SHORT).show();
+    }
+
+    @OnNeverAskAgain(Manifest.permission.CAMERA)
+    void showNeverAskForCamera() {
+        Snackbar.make(findViewById(android.R.id.content), R.string.permission_neverask, Snackbar.LENGTH_SHORT).show();
+    }
+
+    @OnPermissionDenied(Manifest.permission.READ_EXTERNAL_STORAGE)
+    void showDeniedForReadStorage() {
+        Snackbar.make(findViewById(android.R.id.content), R.string.permission_denied, Snackbar.LENGTH_SHORT).show();
+    }
+
+    @OnNeverAskAgain(Manifest.permission.READ_EXTERNAL_STORAGE)
+    void showNeverAskForReadStorage() {
+        Snackbar.make(findViewById(android.R.id.content), R.string.permission_neverask, Snackbar.LENGTH_SHORT).show();
+    }
+
+    @OnPermissionDenied(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+    void showDeniedForWriteStorage() {
+        Snackbar.make(findViewById(android.R.id.content), R.string.permission_denied, Snackbar.LENGTH_SHORT).show();
+    }
+
+    @OnNeverAskAgain(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+    void showNeverAskForWriteStorage() {
+        Snackbar.make(findViewById(android.R.id.content), R.string.permission_neverask, Snackbar.LENGTH_SHORT).show();
+    }
+}
--- app/src/main/java/com/codepath/timeline/fragments/BaseStoryModelFragment.java
+++ app/src/main/java/com/codepath/timeline/fragments/BaseStoryModelFragment.java
@@ -1,9 +1,11 @@
 package com.codepath.timeline.fragments;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -13,8 +15,11 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.codepath.timeline.R;
+import com.codepath.timeline.activities.NewStoryActivity;
 import com.codepath.timeline.adapters.StoriesAdapter;
 import com.codepath.timeline.models.Story;
+
+import org.parceler.Parcels;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -31,6 +36,7 @@ abstract public class BaseStoryModelFragment extends Fragment {
     @BindView(R.id.addBtn) FloatingActionButton add;
     @BindView(R.id.rvStories) RecyclerView rvStories;
     private Unbinder unbinder;
+    private int REQUEST_CODE = 5;
     private boolean clicked = false;
 
     @Override
@@ -52,7 +58,7 @@ abstract public class BaseStoryModelFragment extends Fragment {
         populateList();
 
         // Todo: on click listener is added directly in the adapter, modify it later to include the title of a story as well
-        // Todo: the code below is not needed then
+        // Todo: the code below is not needed then, remove when sure
 //        ItemClickSupport
 //                .addTo(rvStories)
 //                .setOnItemClickListener(
@@ -69,10 +75,13 @@ abstract public class BaseStoryModelFragment extends Fragment {
 //                        }
 //                );
 
+        //
         add.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // Todo: add a new story
+                Intent intent = new Intent(getActivity(), NewStoryActivity.class);
+                startActivityForResult(intent, REQUEST_CODE);
             }
         });
 
@@ -112,6 +121,22 @@ abstract public class BaseStoryModelFragment extends Fragment {
         stories.addAll(newStories);
         // curSize should represent the first element that got added, newItems.size() represents the itemCount
         adaptStories.notifyItemRangeInserted(curSize, newStories.size());
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // Check which request it is that we're responding to
+        if (requestCode == REQUEST_CODE && resultCode == 1) {
+            // Get the URI that points to the selected contact
+            Story story = Parcels.unwrap(data.getParcelableExtra("story"));
+            Snackbar.make(getView(), story.toString(), Snackbar.LENGTH_SHORT).show();
+            addNew(story);
+        }
+    }
+
+    protected void addNew(Story story) {
+        stories.add(story);
+        adaptStories.notifyItemInserted(0);
     }
 
     @Override
